703e70c8b3591393872d88b8b105680dd1954d4c
MightyElemental <minebox30@gmail.com>
2015-09-27 15:34:14 +0100

    edits

--- LDS Wagon Racing/src/net/mightyelemental/ldsracing/wagon/parts/Cart.java
+++ LDS Wagon Racing/src/net/mightyelemental/ldsracing/wagon/parts/Cart.java
@@ -1,5 +1,14 @@
 package net.mightyelemental.ldsracing.wagon.parts;
 
+import org.newdawn.slick.Image;
+
 public class Cart extends WagonPart {
+
+	@Override
+	public Image getDisplayImage() {
+		float x = 250;
+		float y = x * (128.0f / 364.0f);
+		return displayImage.getScaledCopy((int) x, (int) y);
+	}
 
 }
