163931f4e42274eb114d41caf44179abf79e8658
James <minebox30@gmail.com>
2016-04-08 17:24:47 +0100

    Added a word to the ban dictionary

--- Skype Test/src/net/mightyelemental/skype/SkypeOS.java
+++ Skype Test/src/net/mightyelemental/skype/SkypeOS.java
@@ -16,23 +16,24 @@ import com.skype.SkypeException;
 import com.skype.User;
 
 public class SkypeOS implements ChatMessageListener, ChatMessageEditListener {
-
+	
+	
 	public static String user = "wantspam";
-
+	
 	public static Chat chat;
-
+	
 	public Map<String, List<Object>> spamDetector = new HashMap<String, List<Object>>();
-
+	
 	public static String[] bannedWords = { getSFB(new Byte[] { 102, 117, 99, 107 }), getSFB(new Byte[] { 115, 104, 105, 116 }), "(finger)",
-			getSFB(new Byte[] { 99, 117, 110, 116, 115 }), "religion is wrong", getSFB(new Byte[] { 100, 105, 99, 107 }),
-			getSFB(new Byte[] { 118, 97, 103, 105, 110, 97 }), getSFB(new Byte[] { 112, 117, 115, 115, 121 }),
-			getSFB(new Byte[] { 112, 101, 110, 105, 115 }), getSFB(new Byte[] { 99, 117, 109 }),
-			getSFB(new Byte[] { 119, 97, 110, 107, 101, 114 }), getSFB(new Byte[] { 112, 111, 114, 110 }),
-			getSFB(new Byte[] { 115, 101, 120 }), getSFB(new Byte[] { 97, 114, 115, 101 }),
-			getSFB(new Byte[] { 114, 105, 109, 32, 106, 111, 98 }) };
-
+		getSFB(new Byte[] { 99, 117, 110, 116, 115 }), "religion is wrong", getSFB(new Byte[] { 100, 105, 99, 107 }),
+		getSFB(new Byte[] { 118, 97, 103, 105, 110, 97 }), getSFB(new Byte[] { 112, 117, 115, 115, 121 }),
+		getSFB(new Byte[] { 112, 101, 110, 105, 115 }), getSFB(new Byte[] { 99, 117, 109 }),
+		getSFB(new Byte[] { 119, 97, 110, 107, 101, 114 }), getSFB(new Byte[] { 112, 111, 114, 110 }), getSFB(new Byte[] { 115, 101, 120 }),
+		getSFB(new Byte[] { 97, 114, 115, 101 }), getSFB(new Byte[] { 114, 105, 109, 32, 106, 111, 98 }),
+		getSFB(new Byte[] { 100, 105, 99, 116, 97, 116, 111, 114, 115, 104, 105, 112 }) };
+	
 	Random rand = new Random();
-
+	
 	public static String getSFB(Byte[] b) {
 		String temp = "";
 		for (Byte b1 : b) {
@@ -40,10 +41,10 @@ public class SkypeOS implements ChatMessageListener, ChatMessageEditListener {
 		}
 		return temp;
 	}
-
+	
 	public String stringToByte(String message) {
 		String temp = "";
-
+		
 		for (int i = 0; i < message.length(); i++) {
 			if (message.charAt(i) == ' ') {
 				temp += "#" + ((int) (message.charAt(i))) + "# | ";
@@ -51,12 +52,12 @@ public class SkypeOS implements ChatMessageListener, ChatMessageEditListener {
 				temp += ((int) (message.charAt(i))) + ", ";
 			}
 		}
-
+		
 		return temp;
 	}
-
+	
 	public static String prefix = user + "@skype:~ ";
-
+	
 	public SkypeOS() throws SkypeException, InterruptedException {
 		chat = Skype.chat(user);
 		Skype.addChatMessageListener(this);
@@ -64,26 +65,26 @@ public class SkypeOS implements ChatMessageListener, ChatMessageEditListener {
 			Thread.sleep(10);
 		}
 	}
-
+	
 	public static void main(String[] args) throws SkypeException, InterruptedException {
 		new SkypeOS();
 	}
-
+	
 	public void chat(ChatMessage cm, String message, boolean host) throws SkypeException {
 		chat = cm.getChat();
 		// if (chat.getAllMembers().length < 3) { return; }
-
+		
 		prefix = cm.getSenderDisplayName().replaceAll(" ", "-") + "@SkypeOS-Bot:~ ";
-
+		
 		conditionMessage(message);
 		System.out.println("[Skype] '" + message + "' = " + stringToByte(message));
-
+		
 		boolean flag = removeBannedWords(cm, message);
-
+		
 		/*
 		 * if (!host && !flag && message.length() > 6) { reverse(cm, message); }
 		 */
-
+		
 		if (flag) {
 			// cm.getSender().chat().send("[Skype] The use of banned phrases will not be tolerated");
 			// chat.send(cm.getSenderDisplayName() + " has been muted due to the use of banned phrases");
@@ -93,31 +94,31 @@ public class SkypeOS implements ChatMessageListener, ChatMessageEditListener {
 		} else if (message.length() > 1) {
 			System.out.println(cm.getSenderId() + "> " + message);
 		}
-
+		
 		String[] t = Parser.getArgs(message);
-
+		
 		try {
 			Parser.commands(t, cm);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-
+	
 	public void reverse(ChatMessage cm, String message) throws SkypeException {
-
+		
 		String sendTemp = "[Skype] ";
-
+		
 		for (int i = message.length() - 1; i >= 0; i--) {
 			sendTemp += message.charAt(i);
 		}
-
+		
 		if (!removeBannedWords(cm, sendTemp)) {
 			chat.send(sendTemp);
 		} else {
 			chat.send("[Skype] If I were to send message backwords, it would contain a banned phrase. Therefore I am not going to send it");
 		}
 	}
-
+	
 	public static boolean removeBannedWords(ChatMessage cm, String message) {
 		for (String s : bannedWords) {
 			if (message.toLowerCase().contains(s)) {
@@ -130,18 +131,18 @@ public class SkypeOS implements ChatMessageListener, ChatMessageEditListener {
 		}
 		return false;
 	}
-
+	
 	public void conditionMessage(String message) throws SkypeException {
 		if (message.contains("apple")) {
 			chat.send("[Skype] (company) Apple is crap. Apple should go bankrupt. Apple should never have existed");
 		}
-
+		
 		// if (message.contains("james") && !message.contains("[Skype]")) {
 		// chat.send(
 		// "[Skype] James is currently unavailable, please talk to his account directly if you are trying to contact
 		// him, thanks.");
 		// }
-
+		
 		if (message.contains("league")) {
 			chat.send("https://www.youtube.com/watch?v=Xhyuey4xU3Q");
 		}
@@ -154,7 +155,7 @@ public class SkypeOS implements ChatMessageListener, ChatMessageEditListener {
 		if (message.contains("thanks skype")) {
 			chat.send("[Skype] You're welcome.");
 		}
-
+		
 		// Smite commands
 		String messTemp = "";
 		switch (message) {
@@ -184,7 +185,7 @@ public class SkypeOS implements ChatMessageListener, ChatMessageEditListener {
 			chat.send("[Skype] [Meaning] vv" + messTemp);
 		}
 	}
-
+	
 	public boolean isUserSpamming(ChatMessage cm) throws SkypeException {
 		if (spamDetector.containsKey(cm.getSenderId())) {
 			Date t = (Date) spamDetector.get(cm.getSenderId()).get(0);
@@ -203,7 +204,7 @@ public class SkypeOS implements ChatMessageListener, ChatMessageEditListener {
 		}
 		return false;
 	}
-
+	
 	@Override
 	public void chatMessageReceived(ChatMessage cm) throws SkypeException {
 		String message = cm.getContent().toLowerCase();
@@ -221,13 +222,13 @@ public class SkypeOS implements ChatMessageListener, ChatMessageEditListener {
 			}
 		}
 	}
-
+	
 	@Override
 	public void chatMessageSent(ChatMessage cm) throws SkypeException {
 		String message = cm.getContent().toLowerCase();
 		chat(cm, message, true);
 	}
-
+	
 	@Override
 	public void chatMessageEdited(ChatMessage cm, Date d, User u) {
 		try {
@@ -237,5 +238,5 @@ public class SkypeOS implements ChatMessageListener, ChatMessageEditListener {
 			e.printStackTrace();
 		}
 	}
-
+	
 }
