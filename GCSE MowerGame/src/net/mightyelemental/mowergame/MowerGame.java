3fadbd5138d46815c86348d75d5fc57e8aa01d97
MightyElemental <minebox30@gmail.com>
2016-04-18 21:49:55 +0100

    Changed resolution & made fullscreen

--- GCSE MowerGame/src/net/mightyelemental/mowergame/MowerGame.java
+++ GCSE MowerGame/src/net/mightyelemental/mowergame/MowerGame.java
@@ -14,42 +14,41 @@ import net.mightyelemental.mowergame.states.MenuState;
 import net.mightyelemental.mowergame.states.ShopState;
 
 public class MowerGame extends StateBasedGame {
-	
-	
+
 	public static ResourceLoader resLoader = new ResourceLoader();
 	public static Random rand = new Random();
-	
+
 	public static final int STATE_GAME = 0;
 	public static final int STATE_MENU = 1;
 	public static final int STATE_SHOP = 2;
 	public static final int STATE_LOAD = 3;
-	
+
 	public GameState gameState = new GameState(STATE_GAME, rand);
 	public MenuState menuState = new MenuState(STATE_MENU);
 	public ShopState shopState = new ShopState(STATE_SHOP);
 	public LoadState loadState = new LoadState(STATE_LOAD);
-	
+
 	public static final String TITLE = "GCSE Mower Game";
-	
-	public static final int WIDTH = 1312;
-	
+
+	public static final int WIDTH = 1280;
+
 	public static Image NULL_IMAGE;
 	public static boolean fullLoaded = true;
-	
-	public MowerGame( String name ) {
+
+	public MowerGame(String name) {
 		super(name);
-		
-		// for (int i = 0; i <= 1600; i++) {
-		// if (i / 16.0 * 9.0 - Math.round(i / 16.0 * 9.0) == 0) {
-		// System.out.println("Width: " + i + " Height:" + (i / 16.0 * 9.0));
-		// }
-		// }
-		
+
+		for (int i = 0; i <= 1312; i++) {
+			if (i / 16.0 * 9.0 - Math.round(i / 16.0 * 9.0) == 0) {
+				System.out.println("Width: " + i + " Height:" + (i / 16.0 * 9.0));
+			}
+		}
+
 		addState(loadState);
 		addState(gameState);
 		addState(menuState);
 		addState(shopState);
-		
+
 		AppGameContainer appGc;
 		try {
 			appGc = new AppGameContainer(this);
@@ -57,6 +56,8 @@ public class MowerGame extends StateBasedGame {
 			appGc.setTargetFrameRate(60);
 			appGc.setShowFPS(false);
 			appGc.setAlwaysRender(true);
+			appGc.setFullscreen(true);
+			appGc.setShowFPS(true);
 			appGc.start();
 		} catch (SlickException e) {
 			e.printStackTrace();
@@ -64,14 +65,14 @@ public class MowerGame extends StateBasedGame {
 		}
 		NULL_IMAGE = resLoader.loadImage("noImage");
 	}
-	
+
 	public static void main(String[] args) {
 		new MowerGame(TITLE);
 	}
-	
+
 	@Override
 	public void initStatesList(GameContainer gc) throws SlickException {
 		this.enterState(STATE_GAME);
 	}
-	
+
 }
