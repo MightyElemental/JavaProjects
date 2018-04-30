7b07d6556a75f9f97702d878c2055aa4e82c619d
James <James@James-PC.Home>
2018-04-30 22:06:25 +0100

    Improved Loader

--- Windows Game/src/net/mightyelemental/winGame/util/ProgramLoader.java
+++ Windows Game/src/net/mightyelemental/winGame/util/ProgramLoader.java
@@ -14,12 +14,15 @@ public class ProgramLoader {
 		try {
 			JarFileLoader loader = new JarFileLoader(new URL[] {});
 			loader.addFile(path);
-			System.out.println(Arrays.asList(loader.getURLs()));
+			System.out.println("Found Jar: " + Arrays.asList(loader.getURLs()));
+			// System.out.println(new
+			// File(loader.getURLs()[0].toString()).canRead()+"|"+loader.getURLs()[0].toString());
 			Class<?> c = Class.forName("Main", true, loader);
-			Method method = c.getMethod("init", Object.class);
+			Method method = c.getMethod("init", int.class);
 			method.setAccessible(true); /* promote the method to public access */
-			System.out.println(Arrays.asList(method.getParameters()));
+			// System.out.println(Arrays.asList(method.getParameters()));
 			method.invoke(c.newInstance(), OSSettings.VERSION);
+			System.out.println("Successfully added [" + path + "]");
 		} catch (IOException | IllegalArgumentException | NoSuchMethodException | SecurityException | ClassNotFoundException
 				| IllegalAccessException | InvocationTargetException | InstantiationException e) {
 			e.printStackTrace();
