2cb8e591acd2ee34b03303cbfc3b01adefd0ff70
James <James@James-PC.Home>
2018-04-30 19:40:25 +0100

    Added password protection

--- Windows Game/src/net/mightyelemental/winGame/guiComponents/GUITextBox.java
+++ Windows Game/src/net/mightyelemental/winGame/guiComponents/GUITextBox.java
@@ -1,5 +1,7 @@
 package net.mightyelemental.winGame.guiComponents;
 
+import java.util.Arrays;
+
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
@@ -7,45 +9,54 @@ import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
 
 public class GUITextBox extends GUIComponent {
-	
-	
+
 	private static final long serialVersionUID = 7295479133012661247L;
-	
-	private StringBuilder text = new StringBuilder("");
-	
-	public GUITextBox( float x, float y, float width, float height, String uid ) {
+
+	private StringBuilder	text	= new StringBuilder("");
+	private char			passwordSym;
+
+	public GUITextBox(float x, float y, float width, float height, String uid) {
 		super(x, y, width, height, uid);
 	}
-	
+
 	@Override
 	public void onKeyPressed(int key, char c) {
-		if (f.getWidth(text.toString()) < width * 0.95f) {
+		if ( f.getWidth(text.toString()) < width * 0.95f ) {
 			text.append((c + "").replaceAll("[^A-Za-z0-9 -_+=./|\\;:\"'`~!@#$%^&*(){}]", ""));
 		}
-		if (key == 14 && text.length() > 0) {
+		if ( key == 14 && text.length() > 0 ) {
 			text.deleteCharAt(text.length() - 1);
 		}
-		if (key == 211 && text.length() > 0) {
+		if ( key == 211 && text.length() > 0 ) {
 			text.delete(0, text.length());
 		}
 	}
-	
+
 	@Override
 	public void draw(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
 		super.draw(gc, sbg, g);
-		
+
 		g.setColor(Color.black);
-		g.drawString(text.toString() + (isSelected() ? "|" : ""), x + (width / 2) - g.getFont().getWidth(text.toString()) / 2,
-			y + (height / 2) - (g.getFont().getLineHeight() / 2));
+		String textToDraw = text.toString();
+		if ( passwordSym != ' ' ) {
+			char[] count = new char[text.length()];
+			Arrays.fill(count, passwordSym);
+			textToDraw = new String(count);
+		}
+		g.drawString(textToDraw + (isSelected() ? "|" : ""), x + (width / 2) - g.getFont().getWidth(textToDraw) / 2,
+				y + (height / 2) - (g.getFont().getLineHeight() / 2));
 	}
-	
+
 	public String getText() {
 		return text.toString();
 	}
-	
+
 	public void setText(String newText) {
 		text.delete(0, text.length());
 		text.append(newText);
 	}
-	
+
+	public void setPasswordChar(char c) {
+
+	}
 }
