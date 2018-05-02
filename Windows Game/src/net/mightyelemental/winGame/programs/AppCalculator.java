ac9b266b0ff0ce759b4d9901270f26f8d5591e61
James <James@James-PC.Home>
2018-05-02 22:52:00 +0100

    Tested panel system

--- Windows Game/src/net/mightyelemental/winGame/programs/AppCalculator.java
+++ Windows Game/src/net/mightyelemental/winGame/programs/AppCalculator.java
@@ -1,9 +1,10 @@
 package net.mightyelemental.winGame.programs;
 
+import org.newdawn.slick.Color;
 import org.newdawn.slick.Graphics;
 
 import net.mightyelemental.winGame.guiComponents.GUIButton;
-import net.mightyelemental.winGame.guiComponents.GUIEntryPanel;
+import net.mightyelemental.winGame.guiComponents.GUIPanel;
 import net.mightyelemental.winGame.guiComponents.dekstopObjects.AppWindow;
 
 public class AppCalculator extends AppWindow {
@@ -12,8 +13,14 @@ public class AppCalculator extends AppWindow {
 
 	public AppCalculator(float x, float y, float width, float height) {
 		super(x, y, 400, 600, "Calculator");
-		this.addGUIObject(new GUIButton(190, 20, "#x", this).setText("EXE"), 5, 210);
-		this.addGUIObject(new GUIEntryPanel(390 - 2, 200, "#display", this), 5, 5);
+		GUIPanel p = new GUIPanel(400 - 12, 600 - 260, this).setGridLayout(4, 3).setColor(Color.red);
+		// this.addGUIObject(new GUIButton(190, 20, "#x", this).setText("EXE"), 5, 210);
+		// this.addGUIObject(new GUIEntryPanel(390 - 2, 200, "#display", this), 5, 5);
+		this.addGUIObject(p, 5, 210);
+		for ( int i = 1; i <= 9; i++ ) {
+			p.addGUIObject(new GUIButton(190, 20, "#" + i, this).setText("#" + i), 0, 0);
+		}
+		p.addGUIObject(new GUIButton(190, 20, "#x", this).setText("EXE"), 0, 0);
 		this.setSleepTime(50);
 	}
 
