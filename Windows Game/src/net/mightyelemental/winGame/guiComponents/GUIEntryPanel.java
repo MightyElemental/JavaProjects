e705a786c1fbb7be8ff4c728b75869bda95662d1
James Burnell <James Burnell@DESKTOP-THVSG6N>
2018-05-02 16:29:41 +0100

    Started work on entry panel

---
+++ Windows Game/src/net/mightyelemental/winGame/guiComponents/GUIEntryPanel.java
@@ -0,0 +1,47 @@
+package net.mightyelemental.winGame.guiComponents;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.newdawn.slick.Color;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+import net.mightyelemental.winGame.guiComponents.dekstopObjects.AppWindow;
+
+public class GUIEntryPanel extends GUIComponent {
+
+	private static final long serialVersionUID = -8339400027648252407L;
+
+	private List<Object[]> entries = new ArrayList<Object[]>();
+
+	public GUIEntryPanel(float x, float y, float width, float height, String uid) {
+		super(x, y, width, height, uid);
+	}
+
+	public GUIEntryPanel(float width, float height, String uid, AppWindow aw) {
+		super(0, 0, width, height, uid);
+		this.setLinkedWindow(aw);
+	}
+
+	public void addEntry(String text) {
+		addEntry(text, false, null);
+	}
+
+	public void addEntry(String text, boolean onRight) {
+		addEntry(text, onRight, null);
+	}
+
+	public void addEntry(String text, boolean onRight, Color c) {
+		entries.add(new Object[] { text, onRight, c });
+	}
+
+	@Override
+	public void draw(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
+		g.setColor(this.color);
+		g.fill(this);
+	}
+
+}
