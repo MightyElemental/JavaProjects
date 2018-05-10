d02224cac750bbd380181902e4d15facf19b194c
James Burnell <James Burnell@DESKTOP-THVSG6N>
2018-05-10 12:48:22 +0100

    Repositioned buttons

--- Windows Game/src/net/mightyelemental/winGame/programs/AppWebBrowser.java
+++ Windows Game/src/net/mightyelemental/winGame/programs/AppWebBrowser.java
@@ -1,5 +1,6 @@
 package net.mightyelemental.winGame.programs;
 
+import org.newdawn.slick.Color;
 import org.newdawn.slick.Graphics;
 
 import net.mightyelemental.winGame.guiComponents.GUIButton;
@@ -11,9 +12,9 @@ public class AppWebBrowser extends AppWindow {
 	private static final long serialVersionUID = 7935648659277287522L;
 
 	public AppWebBrowser(float x, float y, float width, float height) {
-		super(x, y, 800, 800 / 16f * 9f, "Corner");
-		this.addGUIObject(new GUITextBox(width - 12, 20, "#URL_BAR"), 5, 5);
-		this.addGUIObject(new GUIButton(10, 10, "#test", this), 100, 100);
+		super(x, y, 1000, 1000 / 16f * 9f, "Corner");
+		this.addGUIObject(new GUITextBox(width - 34, 20, "#URL_BAR"), 5, 5);
+		this.addGUIObject(new GUIButton(20, 20, "#go", this).setText("->").setColor(Color.green), width - 25, 5);
 	}
 
 	@Override
