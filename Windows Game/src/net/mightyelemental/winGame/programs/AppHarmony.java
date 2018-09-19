d3cdd03438b8dc9f9ff9b9c847fd017556864f72
James <James@James-PC.Home>
2018-09-19 22:22:52 +0100

    Worked on harmony app

--- Windows Game/src/net/mightyelemental/winGame/programs/AppHarmony.java
+++ Windows Game/src/net/mightyelemental/winGame/programs/AppHarmony.java
@@ -1,7 +1,12 @@
 package net.mightyelemental.winGame.programs;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 import org.newdawn.slick.Graphics;
 
+import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseAuth;
@@ -18,11 +23,19 @@ public class AppHarmony extends AppWindow {
 	public AppHarmony(float x, float y, float width, float height) {
 		super(x, y, width, height, "Harmony");
 
-		FirebaseOptions.Builder builder = new FirebaseOptions.Builder();
-		builder.setDatabaseUrl("https://xendos-chat.firebaseio.com/");
-		fbApp = FirebaseApp.initializeApp(builder.build(), "Harmony");
-		fbAuth = FirebaseAuth.getInstance(fbApp);
-		System.out.println(fbApp.getName());
+		try {
+			//FileInputStream serviceAccount = new FileInputStream("path/to/serviceAccountKey.json");
+			FirebaseOptions options = new FirebaseOptions.Builder().setCredentials(GoogleCredentials.getApplicationDefault())
+					.setDatabaseUrl("https://xendos-chat.firebaseio.com/").build();
+
+			fbApp = FirebaseApp.initializeApp(options);
+			System.out.println(fbApp.getName());
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 	@Override
