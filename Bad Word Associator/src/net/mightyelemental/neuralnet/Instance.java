5c3525126679899bd4ed256585a88875580834b1
James <James@192.168.0.54>
2018-10-04 18:52:33 +0100

    Changed mutation rate

--- Bad Word Associator/src/net/mightyelemental/neuralnet/Instance.java
+++ Bad Word Associator/src/net/mightyelemental/neuralnet/Instance.java
@@ -12,7 +12,7 @@ public class Instance implements Serializable {// TODO: training data. Strength 
 
 	private Node[] nodes;
 
-	private int[] shape = { 7, 8, 8, 2 };
+	private int[] shape = { 8, 11, 11, 11, 2 };
 
 	String word;
 
@@ -20,7 +20,7 @@ public class Instance implements Serializable {// TODO: training data. Strength 
 
 	public int shapeTotal() {
 		int total = 0;
-		for (int i : shape) {
+		for ( int i : shape ) {
 			total += i;
 		}
 		return total;
@@ -31,10 +31,10 @@ public class Instance implements Serializable {// TODO: training data. Strength 
 		nodes = new Node[shapeTotal()];
 		int totalTemp = shape[0];
 		int tempLayer = 0;
-		for (int x = 0; x < nodes.length; x++) {
+		for ( int x = 0; x < nodes.length; x++ ) {
 			Node n = new Node(this);
 			n.ID = x;
-			if (x < totalTemp) {
+			if ( x < totalTemp ) {
 				n.layer = tempLayer;
 			} else {
 				totalTemp += shape[tempLayer + 1];
@@ -44,24 +44,24 @@ public class Instance implements Serializable {// TODO: training data. Strength 
 			nodes[x] = n;
 		}
 		List<List<Node>> layers = new ArrayList<List<Node>>();
-		for (int lay = 0; lay < shape.length; lay++) {
+		for ( int lay = 0; lay < shape.length; lay++ ) {
 			layers.add(Arrays.asList(getNodeLayer(lay)));
 		}
 		Node[] lay1 = layers.get(1).toArray(new Node[layers.get(1).size()]);
-		for (Node n : layers.get(0)) {
+		for ( Node n : layers.get(0) ) {
 			n.addConnections(lay1, true);
 		}
-		for (int lay = 1; lay < layers.size() - 1; lay++) {
+		for ( int lay = 1; lay < layers.size() - 1; lay++ ) {
 			Node[] nods1 = layers.get(lay - 1).toArray(new Node[layers.get(lay - 1).size()]);
 			Node[] nods2 = layers.get(lay + 1).toArray(new Node[layers.get(lay + 1).size()]);
-			for (Node n : layers.get(lay)) {
+			for ( Node n : layers.get(lay) ) {
 				n.addConnections(nods2, true);
 				n.addConnections(nods1, false);
 			}
 		}
 
 		Node[] lay = layers.get(layers.size() - 2).toArray(new Node[layers.get(layers.size() - 2).size()]);
-		for (Node n : layers.get(layers.size() - 1)) {
+		for ( Node n : layers.get(layers.size() - 1) ) {
 			n.addConnections(lay, false);
 		}
 	}
@@ -69,7 +69,7 @@ public class Instance implements Serializable {// TODO: training data. Strength 
 	public Instance(Node[] oldNodes, String word) {
 		this.word = word;
 		this.nodes = new Node[shapeTotal()];
-		for (int x = 0; x < nodes.length; x++) {
+		for ( int x = 0; x < nodes.length; x++ ) {
 			Node n = new Node(this, oldNodes[x].nextLayer, oldNodes[x].previousLayer, oldNodes[x].ID);
 			n.setValue(-10000);
 			n.layer = oldNodes[x].layer;
@@ -79,15 +79,15 @@ public class Instance implements Serializable {// TODO: training data. Strength 
 
 	public void updateInitialNodes(double[] inputVars) {
 		Node[] layer1 = getNodeLayer(0);
-		for (int i = 0; i < inputVars.length; i++) {
+		for ( int i = 0; i < inputVars.length; i++ ) {
 			layer1[i].setValue(inputVars[i]);
 		}
 	}
 
 	public Node[] getNodeLayer(int layer) {
 		List<Node> r = new ArrayList<Node>();
-		for (Node n : nodes) {
-			if (n.layer == layer) {
+		for ( Node n : nodes ) {
+			if ( n.layer == layer ) {
 				r.add(n);
 			}
 		}
@@ -96,12 +96,12 @@ public class Instance implements Serializable {// TODO: training data. Strength 
 
 	public Instance mutate() {
 		Node n = rand.randFromArray(nodes);
-		if (n.nextLayer.size() > 0) {
+		if ( n.nextLayer.size() > 0 ) {
 			int newN = rand.randKeyFromMap(n.nextLayer);
-			n.setWeight(newN, rand.nextBoolInt(n.getWeight(newN), 0.5));// rand.nextDouble() / 4.5
+			n.setWeight(newN, rand.nextBoolInt(n.getWeight(newN), rand.nextDouble() / 5.0));// rand.nextDouble() / 5.0
 		} else {
 			int newN = rand.randKeyFromMap(n.previousLayer);
-			n.setWeight(newN, rand.nextBoolInt(n.getWeight(newN), 0.5));
+			n.setWeight(newN, rand.nextBoolInt(n.getWeight(newN), rand.nextDouble() / 5.0));
 		}
 		return this;
 	}
@@ -116,7 +116,7 @@ public class Instance implements Serializable {// TODO: training data. Strength 
 
 	public void updateValues(double[] vals) {
 		double[] vars = new double[shape[0]];
-		for (int i = 0; i < vars.length && i < vals.length; i++) {
+		for ( int i = 0; i < vars.length && i < vals.length; i++ ) {
 			vars[i] = (vals[i] - 96) / 27.0;
 		}
 		/*
@@ -124,16 +124,16 @@ public class Instance implements Serializable {// TODO: training data. Strength 
 		 * (vals[3] - 96) / 27.0, (vals[4] - 96) / 27.0 };
 		 */
 
-		for (int i = 0; i < vars.length; i++) {
-			if (nodes[i].layer != 0) {
+		for ( int i = 0; i < vars.length; i++ ) {
+			if ( nodes[i].layer != 0 ) {
 				System.err.println("asd " + nodes[i].layer);
 			}
-			if (vars[i] < 0 || vars[i] > 1)
+			if ( vars[i] < 0 || vars[i] > 1 )
 				throw new IndexOutOfBoundsException("input var " + i + " is out of bounds (" + vars[i] + ")");
 			nodes[i].setValue(vars[i]);
 		}
-		for (int lay = 1; lay < shape.length; lay++) {
-			for (Node n : getNodeLayer(lay)) {
+		for ( int lay = 1; lay < shape.length; lay++ ) {
+			for ( Node n : getNodeLayer(lay) ) {
 				n.updateValue();
 			}
 		}
@@ -142,8 +142,8 @@ public class Instance implements Serializable {// TODO: training data. Strength 
 	private double[] getChars() {
 		double[] d = new double[5];
 		char[] chars = word.toCharArray();
-		for (int i = 0; i < d.length; i++) {
-			if (i > chars.length - 1) {
+		for ( int i = 0; i < d.length; i++ ) {
+			if ( i > chars.length - 1 ) {
 				d[i] = 96;
 				continue;
 			}
@@ -160,7 +160,7 @@ public class Instance implements Serializable {// TODO: training data. Strength 
 	public double[] result() {
 		updateValues(getChars());
 		double[] resultNodes = new double[shape[shape.length - 1]];
-		for (int i = 0; i < resultNodes.length; i++) {
+		for ( int i = 0; i < resultNodes.length; i++ ) {
 			resultNodes[i] = nodes[nodes.length - 1 - i].getValue();
 		}
 		return resultNodes;
@@ -182,9 +182,9 @@ public class Instance implements Serializable {// TODO: training data. Strength 
 
 	public void printConnections() {
 		System.out.println("Non Zero Connections");
-		for (Node n : nodes) {
+		for ( Node n : nodes ) {
 			// System.out.println(n);
-			if (n.nextLayer.isEmpty()) {
+			if ( n.nextLayer.isEmpty() ) {
 				System.out.println(n.ID + "->" + n.previousLayer);
 			} else {
 				System.out.println(n.ID + "->" + n.nextLayer);
@@ -194,7 +194,7 @@ public class Instance implements Serializable {// TODO: training data. Strength 
 
 	public void printNodes() {
 		System.out.println("Nodes");
-		for (Node n : nodes) {
+		for ( Node n : nodes ) {
 			System.out.println(n);
 		}
 	}
@@ -207,11 +207,10 @@ public class Instance implements Serializable {// TODO: training data. Strength 
 	public void printFitnessAllWords() {
 		double total = 0;
 		int correct = 0;
-		for (String word : WordManager.words.keySet()) {
+		for ( String word : WordManager.words.keySet() ) {
 			this.word = word;
 			double fit = getFitness();
-			if (fit > 0)
-				correct++;
+			if ( fit > 0 ) correct++;
 			total += getFitness();
 		}
 		System.out.println(Main.genNumber + ">> " + correct + " >> " + total);
